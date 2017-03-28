3b5a828e9e3853edc4d7b11e9173641d861a3a4f
ryobg <ryobg@users.noreply.github.com>
2017-03-28 15:18:15 +0300

    Extend hmap2obj to support obj area specification

--- hmap2obj.cpp
+++ hmap2obj.cpp
@@ -44,7 +44,7 @@
 class hmap2obj
 {
 public:
-    /// 32-bit integer based 2d vector
+    /// 32-bit unsigned 2d vector
     typedef std::array<std::uint32_t, 2> uvec2;
     /// Single float based 3d vector
     typedef std::array<float, 3> vec3;
@@ -54,7 +54,9 @@ public:
     {
         std::string hmap;   ///< Input *.* heightmap binary file to read from
         std::string obj;    ///< Output *.obj file to write to
-        uvec2 hmap_size;    ///< Says how big is the integer grid for the heightmap
+        uvec2 hmap_size;    ///< How big is the integer grid of the input heighmap file
+        vec3 obj_blo;       ///< The lowest corner of the obj bounding box
+        vec3 obj_bhi;       ///< The highest corner of the obj bounding box
     };
 
     // 
@@ -72,11 +74,11 @@ public:
     void read_hmap ();
 
     /// Peek how small is the minimum elevation in the read heightmap
-    vec3::value_type const& hmap_min () const {
+    auto hmap_min () const {
         return vmin;
     }
     /// Peek how big is the maximum elevation in the read heightmap
-    vec3::value_type const& hmap_max () const {
+    auto hmap_max () const {
         return vmax;
     }
 
@@ -89,8 +91,8 @@ public:
 private:
     param_type params;      ///< The input to the app
     std::vector<vec3> xyz;  ///< The point cloud data coming from the obj file
-    std::vector<vec3::value_type> grid; ///< The integer XY grid of height values
-    vec3::value_type vmin, vmax;        ///< The min/max elevation data from the imported heightmap
+    std::vector<vec3::value_type> grid; ///< The imported height values in XY order
+    uvec2::value_type vmin, vmax;       ///< The min/max elevation data of the imported heightmap
 };
 
 //--------------------------------------------------------------------------------------------------
@@ -109,23 +111,11 @@ hmap2obj::param_type hmap2obj::parse_cli (std::vector<std::string> const& args)
 
     param_type p;
     p.hmap_size.fill (0);
+    p.obj_blo.fill (numeric_limits<float>::quiet_NaN ());
+    p.obj_bhi.fill (numeric_limits<float>::quiet_NaN ());
 
     for (auto& arg: args) 
     {
-        try
-        {
-            int n = stoi (arg, nullptr, 0);
-            if (n > 0) 
-                for (auto& d: p.hmap_size) if (!d) 
-                {
-                    d = static_cast<unsigned> (n);
-                    break;
-                }
-            continue;
-        }
-        catch (exception&)
-        {}
-
         if (p.hmap.empty ()) 
         {
             p.hmap = arg;
@@ -137,6 +127,47 @@ hmap2obj::param_type hmap2obj::parse_cli (std::vector<std::string> const& args)
             p.obj = arg;
             continue;
         }
+
+        try
+        {
+            bool succ = false;
+            int n = stoi (arg, nullptr, 0);
+            if (n > 0) 
+                for (auto& d: p.hmap_size) if (d <= 0) 
+                {
+                    d = static_cast<unsigned> (n);
+                    succ = true;
+                    break;
+                }
+            if (succ) continue;
+        }
+        catch (exception&)
+        {}
+
+        try
+        {
+            bool succ = false;
+            float x = stof (arg);
+
+            for (size_t i = 0, n = p.obj_blo.size (); i < n; ++i)
+                if (isnan (p.obj_blo[i])) 
+                {
+                    p.obj_blo[i] = x;
+                    succ = true;
+                    break;
+                }
+
+            if (succ) continue;
+
+            for (size_t i = 0, n = p.obj_bhi.size (); i < n; ++i)
+                if (isnan (p.obj_bhi[i])) 
+                {
+                    p.obj_bhi[i] = x;
+                    break;
+                }
+        }
+        catch (exception&)
+        {}
     }
 
     return p;
@@ -153,23 +184,39 @@ hmap2obj::param_type hmap2obj::parse_cli (std::vector<std::string> const& args)
 
 std::string hmap2obj::validate_params (param_type const& p)
 {
+    using namespace std;
+
     if ([&p] () -> bool {
-            std::ifstream f;
+            ifstream f;
             f.open (p.hmap);
             return !f.is_open ();
         } ())
         return "An input heightmap file was not opened!";
 
     if ([&p] () -> bool {
-            std::ofstream f;
-            f.open (p.obj, std::ios_base::app);
+            ofstream f;
+            f.open (p.obj, ios_base::app);
             return !f.is_open ();
         } ())
         return "An output Wavefront *.obj file was not opened!";
 
     for (auto n: p.hmap_size)
-        if (n < 1)
+        if (n <= 0)
             return "The heightmap size parameter is invalid!";
+
+    for (auto n: p.obj_blo)
+        if (!isnormal (n) && n != 0)
+            return "The obj lowest corner parameter is invalid!";
+
+    for (auto n: p.obj_bhi)
+        if (!isnormal (n) && n != 0)
+            return "The obj highest corner parameter is invalid!";
+
+    for (size_t i = 0, n = p.obj_blo.size (); i < n; ++i) 
+    {
+        if (p.obj_blo[i] >= p.obj_bhi[i])
+            return "Obj lowest corner value is greater!";
+    }
 
     return "";
 }
@@ -196,10 +243,9 @@ void hmap2obj::read_hmap ()
     for (size_t i = 0, n = grid.size (); 
             i < n && hmap.read (reinterpret_cast<char*> (&p), sizeof p); ++i, p = 0)
     {
-        float val = p;
-        grid[i] = val;
-        vmin = min (vmin, val);        
-        vmax = max (vmax, val);        
+        grid[i] = p;
+        vmin = min<decltype(vmin)> (vmin, p);        
+        vmax = max<decltype(vmin)> (vmax, p);        
     };
 }
 
@@ -207,6 +253,8 @@ void hmap2obj::read_hmap ()
 
 /**
  * Convert the elevation data to XYZ point cloud.
+ *
+ * For each point we first obtain its percentage location and then remap it to the obj dimension.
  */
 
 void hmap2obj::make_xyz ()
@@ -216,17 +264,17 @@ void hmap2obj::make_xyz ()
     xyz.clear ();
     xyz.resize (grid.size (), { 0.f, 0.f, 0.f });
 
-    for (size_t i = 0, n = xyz.size (); i < n; ++i)
+    for (size_t i = 0, n = grid.size (); i < n; ++i)
     {
-        float x = i % params.hmap_size[0];
-        float z = i / params.hmap_size[1];
-        float y = (grid[i] - vmin) / (vmax - vmin);
+        vec3 pt;
+        pt[0] = (i % params.hmap_size[0]) / (params.hmap_size[0] - 1.f);
+        pt[2] = (i / params.hmap_size[1]) / (params.hmap_size[1] - 1.f);
+        pt[1] = (grid[i] - vmin) / (vmax - vmin);
 
-        x = x / (params.hmap_size[0] - 1) - 0.5f;
-        z = z / (params.hmap_size[1] - 1) - 0.5f;
-        y /= 2;
+        for (size_t j = params.obj_blo.size (); j--; )
+            pt[j] = params.obj_blo[j] + pt[j] * (params.obj_bhi[j] - params.obj_blo[j]);
 
-        xyz[i] = { x, y, z };
+        xyz[i] = pt;
     }
 }
 
@@ -247,14 +295,13 @@ void hmap2obj::dump_obj ()
        file << "v " << v[0] << ' ' << v[1] << ' ' << v[2] << '\n'; 
     }
 
-    size_t xmax = params.hmap_size[0];
-    for (size_t i = 1, n = xyz.size () - xmax; i <= n; ++i)
+    for (size_t i = 1, n = xyz.size () - params.hmap_size[0]; i <= n; ++i)
     {
-        if (i % xmax)
+        if (i % params.hmap_size[0])
         {
             auto v1 = i;
             auto v2 = i + 1;
-            auto v3 = i + xmax;
+            auto v3 = i + params.hmap_size[0];
             file << "f " << v1 << ' ' << v2 << ' ' << v3 << '\n';
 
             v1 = v2;
@@ -278,13 +325,14 @@ int main (int argc, const char* argv[])
     const char* info = 
         "hmap2obj - A binary heightmap convertor to Wavefront *.obj file\n"
         "\n"
-        "hmap2obj HMAP OBJ SIZE_X SIZE_Y\n"
+        "hmap2obj HMAP OBJ SIZE_X SIZE_Y OBJ_LOW_CORNER OBJ_HIGH_CORNER\n"
         "HMAP       - is the output binary heightmap file\n"
         "OBJ        - is the input obj file\n"
         "SIZE_XY    - the two integer dimensions of the heightmap which to put into the obj\n"
+        "OBJ_CORNER - the low/high 3d floating corners of the obj to hold the heightmap\n"
         "\n"
         "Example:\n"
-        "hmap2obj terrain.r16 terrain.obj 4096 4096\n"
+        "hmap2obj terrain.r16 terrain.obj 4096 4096 -0.5 0 -0.5 0.5 0.1 0.5\n"
         ;
     try
     {
